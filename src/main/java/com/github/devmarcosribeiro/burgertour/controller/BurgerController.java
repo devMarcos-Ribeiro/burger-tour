f0dde5ad27c63494690619faee2dedff9b62c6dd
Marcos Ribeiro <marcosgrsouto@gmail.com>
2022-04-06 20:20:04 -0300

    fixes burger controller path

--- src/main/java/com/github/devmarcosribeiro/burgertour/controller/BurgerController.java
+++ src/main/java/com/github/devmarcosribeiro/burgertour/controller/BurgerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/v1/burgers")
+@RequestMapping("/burgers")
 public class BurgerController {
 
     @GetMapping
